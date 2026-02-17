431841d81f899b78b54c6ac5fe03e2a6add3d636
sn0936-max <sn0936@srmist.edu.in>
2026-02-17 12:59:51 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 /**
- * OOPSBannerApp
- * UC1: Print OOPS to Console
+ * OOPSBannerApp-UC2
+ * UC2: Print OOPS in banner format using * and spaces
  *
  * @author Saksh
  * @version 1.0
@@ -8,26 +8,12 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        // Line 1
         System.out.println("  *****   " + "  *****   " + "  ******  " + "   *****  ");
-
-        // Line 2
         System.out.println(" **   **  " + " **   **  " + " **   **  " + "  **      ");
-
-        // Line 3
         System.out.println(" **   **  " + " **   **  " + " **   **  " + "   ***    ");
-
-        // Line 4
         System.out.println(" **   **  " + " **   **  " + " ******   " + "      **  ");
-
-        // Line 5
         System.out.println(" **   **  " + " **   **  " + " **       " + "      **  ");
-
-        // Line 6
         System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
-
-        // Line 7
         System.out.println("  *****   " + "  *****   " + " **        " + "  *****   ");
 
     }
